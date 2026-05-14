e6bba147048cdaaa0ab6ba5cc0fad6d057690c7c
RomanShtykov <xxxromaromaxxx@gmail.com>
2026-05-14 15:18:33 +0200

    added db migration on start up

--- src/Catalog.Api/Program.cs
+++ src/Catalog.Api/Program.cs
@@ -35,6 +35,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+    await dbContext.Database.MigrateAsync();
     await CatalogDbSeeder.SeedAsync(dbContext);
 }
 
--- src/Order.Api/Program.cs
+++ src/Order.Api/Program.cs
@@ -44,6 +44,12 @@ builder.Services.AddHealthChecks()
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
